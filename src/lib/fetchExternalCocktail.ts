7e3daaa189bd53b63e5132fc5c0ace98a91bc1cb
lfehrenbach <lfehrenbach@foodfightinc.com>
2025-04-30 17:54:31 -0500

    updating liquor type filter

--- src/lib/fetchExternalCocktail.ts
+++ src/lib/fetchExternalCocktail.ts
@@ -3,7 +3,7 @@
 export async function fetchExternalCocktailFromGoogle(query: string) {
     const apiKey = process.env.GOOGLE_CUSTOM_SEARCH_API_KEY!;
     const cx = process.env.GOOGLE_CUSTOM_SEARCH_CX_ID!;
-    const searchUrl = `https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(query)}&key=${apiKey}&cx=${cx}`;
+    const searchUrl = `https://www.googleapis.com/customsearch/v1?key=AIzaSyAXXE6jfJpxCWiQ6gwrhK8mlqBGRrDRJ6M&cx=b5f886c1f6ae3413b&q=test`;
   
     const res = await fetch(searchUrl);
     if (!res.ok) {
